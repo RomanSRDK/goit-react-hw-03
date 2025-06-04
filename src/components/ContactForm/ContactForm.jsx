646f1a02c948328176b3957c21555406cb78e897
Roman Serdiuk <roman.srdk21@gmail.com>
2025-06-04 16:10:44 -0400

    step_3.1_Done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,65 @@
+import { Formik, Form, Field } from "formik";
+import { object, string } from "yup";
+import { useId } from "react";
+import styles from "./ContactForm.module.css";
+
 function ContactForm() {
-  return <div></div>;
+  let userSchema = object({
+    name: string().required().min(3).max(50),
+    number: string().required().max(9),
+  });
+
+  const nameFieldId = useId();
+  const emailFieldId = useId();
+
+  const initialValues = {
+    name: "",
+    number: "",
+  };
+
+  const handleSubmit = (values, { resetForm }) => {
+    if (!Object.values(values).every((value) => value.trim() !== "")) {
+      return;
+    }
+    console.log(values);
+    resetForm();
+  };
+
+  return (
+    <>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Form className={styles.form}>
+          <div className={styles.fieldGroup}>
+            <label htmlFor={nameFieldId} className={styles.label}>
+              Name
+            </label>
+            <Field
+              id={nameFieldId}
+              name="name"
+              type="text"
+              className={styles.input}
+            />
+          </div>
+
+          <div className={styles.fieldGroup}>
+            <label htmlFor={emailFieldId} className={styles.label}>
+              Number
+            </label>
+            <Field
+              id={emailFieldId}
+              name="number"
+              type="text"
+              className={styles.input}
+            />
+          </div>
+
+          <button type="submit" className={styles.button}>
+            Add contact
+          </button>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
 export default ContactForm;
